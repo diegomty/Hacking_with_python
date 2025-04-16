262bcfc334a7e26933a368d517ccb066212fb9a3
diegomty <jperez256@alumnos.uaq.mx>
2025-04-16 00:07:31 -0600

    Creacion de cliente UDP y Creacion de Servidor

--- TCP Client.py
+++ TCP Client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "127.0.0.1"
+target_port = 9998
 
 #create a socket  object
 #El parametro AF_INET indica que se va a utilizar IPv4 y SOCK_STREAM indica que se va a utilizar TCP
@@ -11,9 +11,14 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
 
 #Send some data
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"ABCDEF")
 
 #Receive some data
 response = client.recv(4096)
-print(response.decode())
+print(f"Respuesta del servidor: {response.decode()}")
+#Close the client
 client.close()
+
+#Para probar la  conexion, primero ejecutamos el servidor  en una terminal y luego el cliente en otra terminal
+#Para ejecutar el cliente, ejecutamos el siguiente comando en la terminal: python TCP_Client.py
+#Para ejecutar el servidor, ejecutamos el siguiente comando en la terminal: python TCP_Server.py
